3cfef53693d0ae39d6a4e5bdd6dd101773208659
Mourad-Ouerghi <mouraouerghi12@gmail.com>
2021-07-22 22:39:50 +0100

    removed the sort button and added a graph for continents

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 //fetching the data from the api
 const covidApi='https://corona.lmao.ninja/v2/countries?yesterday=true&sort';
+const worldCovidApi='https://disease.sh/v3/covid-19/all';
+const continentApi='https://corona.lmao.ninja/v2/continents?yesterday=true&sort';
 async function getData(url)
 {
     const fetchResults = await fetch(url);
@@ -10,8 +12,98 @@ var output;
 var countryTable=[];
 
 let data=getData(covidApi);
-//displaying few tunisian stats
-showFewDetails('tunisiaStats',data,"tnTable","Tunisia");
+let worldData=getData(worldCovidApi);
+let continentData=getData(continentApi);
+
+
+    worldData.then((element)=>{
+        output=`<table class="table text-center" id='worldCard' ">`
+            
+                let textColors=assignTextColors(element.todayCases,element.todayDeaths,element.todayRecovered);
+                output+=`
+                <tr>
+                    <th style=${textColors["casesTextColor"]}>
+                        +${element.todayCases}<br>
+                        Cases
+                    </th>
+                    <th style=${textColors["deathsTextColor"]}>
+                        +${element.todayDeaths}<br>
+                        Deaths
+                    </th>
+                </tr>
+                <tr>
+                    <th style=${textColors["recoveredTextColor"]}>
+                        +${element.todayRecovered}<br>
+                        Recovered
+                    </th>
+                    <th>
+                        ${element.critical}<br>
+                        critical
+                    </th>
+                </tr>
+                </table>
+                `
+            document.getElementById('worldStats').innerHTML=output;
+    })
+
+function displayMoreWorldData()
+{
+    worldData.then((element)=>{
+        let output=`<table class="table text-center" id="worldCard" style="color:whitesmoke;">`
+        
+            
+                let textColors=assignTextColors(element.todayCases,element.todayDeaths,element.todayRecovered);
+    
+                output+=`
+                <tr>
+                    <th style=${textColors["casesTextColor"]}>
+                        +${element.todayCases}<br>
+                        Cases
+                    </th>
+                    <th style=${textColors["deathsTextColor"]}>
+                        +${element.todayDeaths}<br>
+                        Deaths
+                    </th>
+                    <th style=${textColors["recoveredTextColor"]}>
+                        +${element.todayRecovered}<br>
+                        Recovered
+                        
+                    </th>
+                </tr>
+                <tr>
+                    <th>
+                        ${element.cases}<br>
+                        Total Cases
+                    </th>
+                    <th>
+                        ${element.deaths}<br>
+                        Total Deaths
+                    </th>
+                    <th>
+                        ${element.recovered}<br>
+                        Total Recovered
+                    </th>
+                </tr>
+                <tr>
+                    <th>
+                        ${element.active}<br>
+                        Active Cases
+                    </th>
+                    <th>
+                        ${element.tests}<br>
+                        Tested
+                    </th>
+                    <th>
+                        ${element.critical}<br>
+                        critical
+                    </th>
+                </tr>
+                </table>
+                `
+            document.getElementById('worldStats').innerHTML=output;
+    })
+}
+//displayWorldData();
 //search Modal
 let searchClicked=false;
 document.getElementById('searchForm').addEventListener('submit',searchCountry);
@@ -35,7 +127,9 @@ function searchCountry(e)
                 let alertDiv=document.getElementById("alert");
                 let countryDiv=document.getElementById("random-country");
                 if(countryDiv!=null)
-                countryDiv.remove();
+                {countryDiv.remove();
+                document.getElementById('textModal').remove();
+            }
                 else
                 alertDiv.remove();
             }
@@ -45,6 +139,7 @@ function searchCountry(e)
             p.innerHTML='For more details check the table below';
             p.style.color="black";
             p.style.fontWeight="bold";
+            p.setAttribute('id','textModal');
             showFewDetails("modal-container",data,"random-country",searchValue);
             document.getElementById("pContainer").appendChild(p);
         }
@@ -59,113 +154,8 @@ function searchCountry(e)
         searchClicked=true;
     })
 }
-let buttonChecked=0;
-let buttonUnchecked=0;
-//let worldTableNotSorted=document.getElementById('wTable');
-document.getElementById('checkboxButton').addEventListener('click',sortTable);
-function sortTable()
-{
-    if(buttonChecked==buttonUnchecked)
-    buttonChecked++;
-    else if(buttonUnchecked<buttonChecked)
-    buttonUnchecked++;
 
-    if(buttonChecked>buttonUnchecked)
-    {
-        document.getElementById('wTable').remove();
-        
-        data.then(
-            
-            (covidData)=>{
-                let output=`<table id="wTable" class="table table-bordered table-hover text-center table-dark table-striped">
-                <tr>
-                <th>countries</th>
-                <th>Cases</th>
-                <th>Today Cases</th>
-                <th>Deaths</th>
-                <th>Today Deaths</th>
-                <th>Recovered</th>
-                <th>Today Recovered</th>
-                <th>Active</th>
-                <th>Critical</th>
-                <th>Tested</th>
-                <th>Population</th>
-                </tr>
-                        `
-                covidData.sort((element1,element2)=>{
-                return element2.cases-element1.cases;
-                })
-                covidData.forEach((element)=>
-                {
-                    let textColors=assignTextColors(element.todayCases,element.todayDeaths,element.todayRecovered);
-                    output+=`
-                    <tr>
-                        <td id="${element.country}">${element.country}<br>
-                        <img src="https://www.countryflags.io/${element.countryInfo.iso2}/shiny/64.png"></td>
-                        <td>${element.cases}</td>
-                        <td style=${textColors["casesTextColor"]}>${element.todayCases}</td>
-                        <td>${element.deaths}</td>
-                        <td style=${textColors["deathsTextColor"]}>${element.todayDeaths}</td>
-                        <td>${element.recovered}</td>
-                        <td style=${textColors["recoveredTextColor"]}>${element.todayRecovered}</td>
-                        <td>${element.active}</td>
-                        <td>${element.critical}</td>
-                        <td>${element.tests}</td>
-                        <td>${element.population}</td>
-                    </tr>
-                    `
-                })
-                output+='</table>';
-                document.getElementById('tableContainer').innerHTML=output;
-            }
-        )
-    }
-    else if(buttonChecked==buttonUnchecked)
-    {
-        console.log('unchecked');
-        document.getElementById('wTable').remove();
-        //let worldTable=document.getElementById('wTable')
-        data.then((covidData)=>{
-            let output=`<table id="wTable" class="table table-bordered table-hover text-center table-dark table-striped">
-                <tr>
-                <th>countries</th>
-                <th>Cases</th>
-                <th>Today Cases</th>
-                <th>Deaths</th>
-                <th>Today Deaths</th>
-                <th>Recovered</th>
-                <th>Today Recovered</th>
-                <th>Active</th>
-                <th>Critical</th>
-                <th>Tested</th>
-                <th>Population</th>
-                </tr>
-            `
-            covidData.forEach(element => {
-                let textColors=assignTextColors(element.todayCases,element.todayDeaths,element.todayRecovered);
-                output+=`
-                <tr>
-                    <td id="${element.country}">${element.country}<br>
-                    <img src="https://www.countryflags.io/${element.countryInfo.iso2}/shiny/64.png"></td>
-                    <td>${element.cases}</td>
-                    <td style=${textColors["casesTextColor"]}>${element.todayCases}</td>
-                    <td>${element.deaths}</td>
-                    <td style=${textColors["deathsTextColor"]}>${element.todayDeaths}</td>
-                    <td>${element.recovered}</td>
-                    <td style=${textColors["recoveredTextColor"]}>${element.todayRecovered}</td>
-                    <td>${element.active}</td>
-                    <td>${element.critical}</td>
-                    <td>${element.tests}</td>
-                    <td>${element.population}</td>
-                </tr>
-                `
-            });
-            output+='</table>'
-            document.getElementById('tableContainer').innerHTML=output;
-        
-        })
-    }
-}
+
 
 //colors assignment
 function assignTextColors(tCases,tDeaths,tRecovered)
@@ -226,8 +216,14 @@ function showFewDetails(divId,donnees,tableId,country)
 
 //More Details button
 document.getElementById('detailsButton').addEventListener('click',addDetails);
-let tnStatsTable=document.getElementById('tnTable');
+let worldStatsTable=document.getElementById('worldCard');
 let moreButton=document.getElementById('detailsButton');
+function addDetails()
+{
+    document.getElementById('worldCard').remove();
+    displayMoreWorldData();
+    lessDetailsButton();
+}
 function lessDetailsButton()
 {
     document.getElementById('detailsButton').remove();
@@ -241,8 +237,8 @@ function lessDetailsButton()
 }
 function showLess()
 {
-    document.getElementById('tnTable').remove();
-    document.getElementById('tunisiaStats').innerHTML=output;
+    document.getElementById('worldCard').remove();
+    document.getElementById('worldStats').innerHTML=output;
     document.getElementById('lessDetails').remove();
     document.getElementById('divButton').appendChild(moreButton);
 }
@@ -306,33 +302,30 @@ function displayElementDetails(country,donnees,divId)
     });
 })
 }
-function addDetails()
-{
-    document.getElementById('tnTable').remove();
-    displayElementDetails("Tunisia",data,"tunisiaStats");
-    lessDetailsButton();
-}
 
-//the world stats table 
-function diplayWorldTableNotSorted()
-{
-    data.then((covidData)=>{
-        let output=`<table id="wTable" class="table table-bordered table-hover text-center table-dark table-striped">
-            <tr>
-            <th>countries</th>
-            <th>Cases</th>
-            <th>Today Cases</th>
-            <th>Deaths</th>
-            <th>Today Deaths</th>
-            <th>Recovered</th>
-            <th>Today Recovered</th>
-            <th>Active</th>
-            <th>Critical</th>
-            <th>Tested</th>
-            <th>Population</th>
-            </tr>
-        `
-        covidData.forEach(element => {
+
+data.then(
+    (covidData)=>{
+        let output=`<table id="wTable" class="table table-bordered  text-center table-dark table-striped">
+        <tr>
+        <th>countries</th>
+        <th>Cases</th>
+        <th>Today Cases</th>
+        <th>Deaths</th>
+        <th>Today Deaths</th>
+        <th>Recovered</th>
+        <th>Today Recovered</th>
+        <th>Active</th>
+        <th>Critical</th>
+        <th>Tested</th>
+        <th>Population</th>
+        </tr>
+                `
+        covidData.sort((element1,element2)=>{
+        return element2.cases-element1.cases;
+        })
+        covidData.forEach((element)=>
+        {
             let textColors=assignTextColors(element.todayCases,element.todayDeaths,element.todayRecovered);
             output+=`
             <tr>
@@ -350,11 +343,56 @@ function diplayWorldTableNotSorted()
                 <td>${element.population}</td>
             </tr>
             `
-        });
-        output+='</table>'
+        })
+        output+='</table>';
         document.getElementById('tableContainer').innerHTML=output;
+    }
+)
+let continentsTable=[['Continent',' Deaths','Recovered','Population']];
+continentData.then((covidData)=>{
+    covidData.forEach((element)=>{
+        continentsTable.push([element.continent,element.deaths,element.recovered,element.population]);
+    })
+        google.charts.load("current", {packages:["corechart"]});
+        google.charts.setOnLoadCallback(drawChart);
+        function drawChart() {
+
+            var data = google.visualization.arrayToDataTable(continentsTable);
+      
+            var options = {
+              title: 
+               `X=Deaths 
+               Y=Recovered 
+               Bubble size=Population 
+               Bubble color=Population`,
+               chartArea: {
+                backgroundColor: {
+                  fill: '#837F7F',
+                  fillOpacity: 0.1
+                },
+              },
+              
+              backgroundColor: {
+                fill: '#212529',
+                fillOpacity: 0.8
+              },
+              
+              colorAxis: {colors: ['#50C878', '#228B22', '#023020']},
+              hAxis: {title: 'Deaths Rate'},
+              vAxis: {title: 'Recovering rate'},
+              bubble: {
+                textStyle: {
+                  auraColor: 'none',
+                }
+              }
+            };
+      
+            var chart = new google.visualization.BubbleChart(document.getElementById('continents'));
+      
+            chart.draw(data, options);
+          }
     
-    });
-}
-if(buttonChecked==0 && buttonUnchecked==0)
-diplayWorldTableNotSorted();
+})
+
+
+    
